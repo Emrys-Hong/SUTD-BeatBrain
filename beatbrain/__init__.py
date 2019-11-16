4458c076253d6a0c67c5153dd9a12632766a172f
Krishna Penukonda <penukonda.krishna.moorthy@gmail.com>
2019-11-16 20:31:38 +0700

    Added Metrics, Display modules; Refactored utils

--- beatbrain/__init__.py
+++ beatbrain/__init__.py
@@ -1,2 +1,5 @@
-from . import utils, display, settings
+from . import utils
+from . import metrics
+from . import display
+from . import settings
 from . import fma
--- beatbrain/display.py
+++ beatbrain/display.py
@@ -1,15 +1,29 @@
 import matplotlib.pyplot as plt
+import seaborn as sns
 import numpy as np
 import librosa
-import librosa.display
 
 
-def show_spec(
-    spec, sr, y_axis="log", scale_fn=librosa.amplitude_to_db, title=None, **kwargs
-):
-    log_spec = scale_fn(spec, ref=np.max)
-    plt.figure(figsize=(12, 4))
-    librosa.display.specshow(log_spec, sr=sr, x_axis="time", y_axis=y_axis, **kwargs)
-    plt.colorbar(format="%+2.0f dB")
-    if title:
-        plt.title(title)
+def show_spec(spec, scale_fn=librosa.power_to_db, title=None, clean=True, **kwargs):
+    """
+    Display a spectrogram
+
+    Args:
+        spec (np.ndarray): The spectrogram to display
+        scale_fn (function): A function that scales the spectrogram. If false, no scaling is performed
+        title (str): The title of the plot
+        clean (bool): If True, removes axis labels, colorbar, etc.
+        **kwargs (dict): Additional keyword arguments passed to `sns.heatmap`
+    """
+    if scale_fn:
+        spec = scale_fn(spec, ref=np.max)
+    kwargs["cmap"] = kwargs.get("cmap") or "magma"
+    sns.heatmap(
+        spec[::-1],
+        cbar_kws={"format": "%+2.0f dB"},
+        xticklabels=not clean,
+        yticklabels=not clean,
+        cbar=not clean,
+        **kwargs
+    )
+    plt.title(title)
